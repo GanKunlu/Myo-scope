25ba60a62a367c0b29406299fe19a1a133f01800
GanKunlu <Amadeus_Gan@163.com>
2016-06-04 20:31:35 +0800

    Update Myo_scope.py

--- Myo_scope.py
+++ Myo_scope.py
@@ -20,7 +20,6 @@ class AnimationHandler(Handler):
 		info.object.timer.Stop()		
 
 class MYOGrapher(HasTraits):
-	
 	sEMG_length = Range(100,600,150)
 	Angle_length = Range(80,300,120)
 	sEMG_names = List(Str)
@@ -35,39 +34,39 @@ class MYOGrapher(HasTraits):
 	stream_emg = List(deque)
 	stream_Angle = List(deque)
 	traits_view=View(	
-				HSplit(# HSplit分为左右两个区域，中间有可调节宽度比例的调节手柄
-					   # 左边为一个组		
-						VSplit(
-							VGroup(
-								Item("Which_Myo_sEMG", label=u"肌电信号源 "),
-								Item("sEMG_length", label=u"肌电横轴范围 " ),
-								Item("select_emg_names",style="custom", label=u"肌电通道选择 ",
-								editor=CheckListEditor(name="object.sEMG_names", 
-									cols=2, format_str=u"%s")),
-								show_border = True, # 显示组的边框
-								scrollable = True,  # 组中的控件过多时，采用滚动条
-								),
-							VGroup(	
-								Item("Angle_length", label=u"角度横轴范围 "),
-								Item("select_angle_names",style="custom", label=u"选择手环 ",
-								editor=CheckListEditor(name="object.Angle_names", 
-									cols=2, format_str=u"%s")),
-								show_border = True, # 显示组的边框
-								scrollable = True,  # 组中的控件过多时，采用滚动条
-								)
-							),
-						VGroup(
-							Item("plot_EMG", editor = ComponentEditor(),show_label=False,width=700, height=100),
-							Item("plot_Angle", editor = ComponentEditor(),show_label=False,width=700, height=100),
-							show_border = True, # 显示组的边框
-							)
+			HSplit(# HSplit分为左右两个区域，中间有可调节宽度比例的调节手柄
+				 # 左边为一个组		
+				VSplit(
+					VGroup(
+						Item("Which_Myo_sEMG", label=u"肌电信号源 "),
+						Item("sEMG_length", label=u"肌电横轴范围 " ),
+						Item("select_emg_names",style="custom", label=u"肌电通道选择 ",
+						editor=CheckListEditor(name="object.sEMG_names", 
+							cols=2, format_str=u"%s")),
+						show_border = True, # 显示组的边框
+						scrollable = True,  # 组中的控件过多时，采用滚动条
 						),
-					resizable = True,
-					height = 500,
-					width = 1000,
-					title = u"MYO数据信号示波器",
-					handler = AnimationHandler()
+					VGroup(	
+						Item("Angle_length", label=u"角度横轴范围 "),
+						Item("select_angle_names",style="custom", label=u"选择手环 ",
+						editor=CheckListEditor(name="object.Angle_names", 
+							cols=2, format_str=u"%s")),
+						show_border = True, # 显示组的边框
+						scrollable = True,  # 组中的控件过多时，采用滚动条
+						)
+					),
+				VGroup(
+					Item("plot_EMG", editor = ComponentEditor(),show_label=False,width=700, height=100),
+					Item("plot_Angle", editor = ComponentEditor(),show_label=False,width=700, height=100),
+					show_border = True, # 显示组的边框
 					)
+				),
+				resizable = True,
+				height = 500,
+				width = 1000,
+				title = u"MYO数据信号示波器",
+				handler = AnimationHandler()
+			)
 		
 	def __init__(self,**traits):
 		super(MYOGrapher, self).__init__(**traits)
@@ -75,30 +74,25 @@ class MYOGrapher(HasTraits):
 		data_EMG = ArrayPlotData(x=[0],sEMG1=[0],sEMG2=[0],sEMG3=[0],
 			sEMG4=[0],sEMG5=[0],sEMG6=[0],sEMG7=[0],sEMG8=[0])
 		data_Angle = ArrayPlotData(x=[0],myo1=[0],myo2=[0])
-		
 		plot_Angle = Plot(data_Angle)
 		plot_EMG = Plot(data_EMG)
-		
 		plot_EMG.plot(("x","sEMG1"),type="line")
 		plot_EMG.title = "sEMG"
 		plot_EMG.value_scale = 'linear'
-		
 		plot_Angle.plot(("x","myo1"),type="line")
 		plot_Angle.title = "Angle"
 		plot_Angle.value_scale = 'linear'
 		
 		self.plot_EMG = plot_EMG
 		self.data_EMG = data_EMG
-		
 		self.plot_Angle = plot_Angle
 		self.data_Angle = data_Angle
-		
 		self.sEMG_names = ['sEMG1','sEMG2','sEMG3',
 			'sEMG4','sEMG5','sEMG6','sEMG7','sEMG8']
 		self.Angle_names = ['myo1','myo2']
-		
 		_stream_emg = []
 		_stream_angle = []
+		
 		for chanel in range(8):
 			_stream_emg.append(deque(np.zeros(self.sEMG_length)))
 		self.stream_emg = _stream_emg
